4d7bad999a925d9ab9ac99b234099d3190cfc0a1
xiaolin3325 <feelinglucky@qq.com>
2026-05-28 20:36:31 +0800

    cleanup test files

--- check_repo.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import requests, json
-r = requests.get('https://api.github.com/repos/xiaolin3325/trading-system')
-d = r.json()
-if 'message' in d:
-    print(f'Error: {d["message"]}')
-else:
-    print(f'OK: {d["full_name"]} - {d["html_url"]}')
